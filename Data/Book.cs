99e55c49ce87b6575fc18545960b82fbd003307f
Alenas Kisonas <alenas@hotmail.com>
2020-04-24 01:47:10 +0300

    Add project files.

---
+++ Data/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data {
+
+    public class Book {
+        public string ASIN { get; set; }
+        public string IMAGE_URL { get; set; }
+        public string TITLE { get; set; }
+        public string AUTHOR { get; set; }
+        public int CATEGORY_ID { get; set; }
+        public string CATEGORY { get; set; }
+        public Guid ID { get; set; }
+        public double PRICE { get; set; }
+    }
+}
---
+++ StoreAPI/GetBooks.cs
@@ -0,0 +1,15 @@
+namespace StoreAPI {
+    //public static class GetBooks {
+    //    [FunctionName("GetBooks2")]
+    //    public static void Run([CosmosDBTrigger(
+    //        databaseName: "BookStoreDB",
+    //        collectionName: "Books",
+    //        ConnectionStringSetting = "DBConnection",
+    //        LeaseCollectionName = "leases")]IReadOnlyList<Document> input, ILogger log) {
+    //        if (input != null && input.Count > 0) {
+    //            log.LogInformation("Documents modified " + input.Count);
+    //            log.LogInformation("First document Id " + input[0].Id);
+    //        }
+    //    }
+    //}
+}
